f4ef00808b70eb115b55556d22bdcaff68ecc942
BAEKDODAM <56225917+BAEKDODAM@users.noreply.github.com>
2023-12-12 12:21:50 +0900

    init: project setting

---
+++ src/main/java/com/ImageTrip/ImageTripApplication.java
@@ -0,0 +1,13 @@
+package com.ImageTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageTripApplication.class, args);
+	}
+
+}
