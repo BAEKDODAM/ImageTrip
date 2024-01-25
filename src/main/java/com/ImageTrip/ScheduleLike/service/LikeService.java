ceb4d70c60ccca6193a1a0bfd06c3a41b7982252
BAEKDODAM <56225917+BAEKDODAM@users.noreply.github.com>
2024-01-25 23:00:35 +0900

    feat: like 생성, 삭제, 검증

--- src/main/java/com/ImageTrip/ScheduleLike/service/LikeService.java
+++ src/main/java/com/ImageTrip/ScheduleLike/service/LikeService.java
@@ -1,28 +1,59 @@
 package com.ImageTrip.ScheduleLike.service;
 
+import com.ImageTrip.Schedule.entity.Schedule;
+import com.ImageTrip.ScheduleLike.entity.ScheduleLike;
 import com.ImageTrip.ScheduleLike.repository.LikeRepository;
+import com.ImageTrip.exception.BusinessLogicException;
+import com.ImageTrip.exception.ExceptionCode;
+import com.ImageTrip.member.entity.Member;
+import com.ImageTrip.member.repository.MemberRepository;
+import com.ImageTrip.member.service.MemberService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class LikeService {
     private final LikeRepository repository;
+    private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
-    public LikeService(LikeRepository repository) {
+    public LikeService(LikeRepository repository, MemberService memberService, MemberRepository memberRepository) {
         this.repository = repository;
+        this.memberService = memberService;
+        this.memberRepository = memberRepository;
     }
 
     public int scheduleLikeCnt(long scheduleId){
         return (int) repository.findAllByScheduleScheduleId(scheduleId).stream().count();
     }
-    /*public int imageLikeCnt(long imageId){
-        return (int) repository.findAllByImageImageId(imageId).stream().count();
-    }*/
 
-    public void createLike(){
+    public void createLike(Schedule schedule, long memberId){
+        if(findLike(schedule.getScheduleId(), memberId)) {
+            throw new BusinessLogicException(ExceptionCode.ALREADY_LIKED);
+        }
+        //Schedule schedule = scheduleService.findVerifiedSchedule(scheduleId);
+        Member member = memberRepository.findById(memberId).orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.UNMATCHED_WRITER));
 
+        ScheduleLike like = new ScheduleLike();
+        like.setSchedule(schedule);
+        like.setMember(member);
+        repository.save(like);
     }
-    public void deleteLikeByScheduleId(long scheduleId){
+    public void deleteLike(long scheduleId, long memberId){
+        if(!findLike(scheduleId, memberId)) {
+            throw new BusinessLogicException(ExceptionCode.LIKE_NOT_FOUND);
+        }
+        repository.deleteByScheduleScheduleIdAndMemberMemberId(scheduleId, memberId);
+    }
+    public void deleteAllLikeByScheduleId(long scheduleId){
         repository.deleteAllByScheduleScheduleId(scheduleId);
     }
     public void deleteLikeByImageId(long imageId){}
+
+    public boolean findLike(long scheduleId, long memberId){
+        return repository.findByScheduleScheduleIdAndMemberMemberId(scheduleId, memberId).isPresent();
+    }
+
 }
