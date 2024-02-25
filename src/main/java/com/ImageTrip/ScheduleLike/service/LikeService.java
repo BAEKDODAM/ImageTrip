07e53276d41ee242f48773634e7f5c7a1f2676fa
Baek Dodam <eheka1000@gmail.com>
2024-02-26 00:43:52 +0900

    feat: getLikeSchedules

--- src/main/java/com/ImageTrip/ScheduleLike/service/LikeService.java
+++ src/main/java/com/ImageTrip/ScheduleLike/service/LikeService.java
@@ -1,26 +1,26 @@
 package com.ImageTrip.ScheduleLike.service;
 
+import com.ImageTrip.Schedule.dto.ScheduleDto;
 import com.ImageTrip.Schedule.entity.Schedule;
 import com.ImageTrip.ScheduleLike.entity.ScheduleLike;
 import com.ImageTrip.ScheduleLike.repository.LikeRepository;
 import com.ImageTrip.exception.BusinessLogicException;
 import com.ImageTrip.exception.ExceptionCode;
 import com.ImageTrip.member.entity.Member;
-import com.ImageTrip.member.repository.MemberRepository;
-import com.ImageTrip.member.service.MemberService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @Service
 public class LikeService {
     private final LikeRepository repository;
-    private final MemberService memberService;
 
-
-    public LikeService(LikeRepository repository, MemberService memberService) {
+    public LikeService(LikeRepository repository) {
         this.repository = repository;
-        this.memberService = memberService;
     }
 
     public int scheduleLikeCnt(long scheduleId){
@@ -51,4 +51,19 @@ public class LikeService {
         return repository.findByScheduleScheduleIdAndMemberMemberId(scheduleId, memberId).isPresent();
     }
 
+    public List<ScheduleDto.ListResponse> findLikeScheduleByPage(long cursor, Pageable pageable, long memberId){
+        List<Schedule> schedules = getLikedSchedules(cursor, pageable,memberId)
+                .stream().map(scheduleLike -> scheduleLike.getSchedule()).collect(Collectors.toList());
+        return schedules.stream().map(schedule -> {
+            int likeCnt = scheduleLikeCnt(schedule.getScheduleId());
+            boolean liked = findLike(schedule.getScheduleId(), memberId);
+            return ScheduleDto.ListResponse.from(schedule, likeCnt, liked);
+        }).collect(Collectors.toList());
+    }
+
+    public List<ScheduleLike> getLikedSchedules(Long cursor, Pageable pageable, long memberId) {
+        return cursor.equals(0L)
+                ? repository.findByMemberMemberIdOrderByScheduleLikeIdDesc(pageable)
+                : repository.findByMemberMemberIdAndScheduleLikeIdLessThanOrderByScheduleLikeIdDesc(memberId, cursor, pageable);
+    }
 }
